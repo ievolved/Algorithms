b1a08b98ac109104bc89ac233b0bf6ecdfa07936
Shawn <shawn@ievolved.io>
2017-01-19 12:33:58 -0800

    [update] more requirements of graph challenge

--- 10. Graphs/undirectedGraph.js
+++ 10. Graphs/undirectedGraph.js
@@ -72,42 +72,7 @@ Graph.prototype.containsEdge = function(start, end) {
 };
 
 Graph.prototype.addEdge = function(start, end) {
-  var first = this.node_list.contains(start);
-  var second = this.node_list.contains(end);
-
-  if(first){
-    //get start node
-    var i = this.node_list.length;
-    while (i--) {
-      if (this.node_list[i].name === start) {
-        this.node_list[i].addEdge(end);
-        break;
-      }
-    }
-  }
-  if(second){
-    //get end node
-    i = this.node_list.length;
-    while (i--) {
-      if (this.node_list[i].name === end) {
-        this.node_list[i].addEdge(start);
-        break;
-      }
-    }
-  }
-
-  if( (!first) || (!second) ){
-    if( !first ){
-      var node = new Node(start);
-      node.addEdge(end);
-      this.node_list.push(node);
-    }
-    if( !second ){
-      var node = new Node(end);
-      node.addEdge(start);
-      this.node_list.push(node);
-    }
-  }
+  ...
 };
 
 Graph.prototype.removeEdge = function(start, end) {
@@ -116,10 +81,7 @@ Graph.prototype.removeEdge = function(start, end) {
 
 
 Graph.prototype.printNodes = function() {
-  for(var i = 0;i < this.node_list.length;i++){
-    console.log(this.node_list[i].name +":");
-    console.log(this.node_list[i].edge_list);
-  }
+  ...
 };
 
 
