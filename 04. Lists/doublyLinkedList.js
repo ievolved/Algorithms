1d2eac2c0c0f4cbf4ab55b45a5362c6aaeed0fe4
Shawn <shawn@ievolved.io>
2017-02-21 10:05:19 -0800

    [update] Updates to arrays, vectors, and lists

--- 04. Lists/doublyLinkedList.js
+++ 04. Lists/doublyLinkedList.js
@@ -1,6 +1,8 @@
 
 // [ ] Implement DoublyLinkedList class
 // [ ] .insert() function to insert data at position (0 = Head, null = Tail, other = middle somewhere)
+//    [ ] .insertAfter() function to insert data after the node passed in
+//    [ ] .insertBefore() function to insert data before the node passed in
 // [ ] .remove() function to remove data at position (0 = Head, empty = Tail, other = middle somewhere)
 // [ ] .get() function to return data at position indicated
 // [ ] .set() function to change existing data at position indicated
@@ -29,6 +31,14 @@ var DoublyLinkedList = function() {
 
 
 LinkedList.prototype.insert = function(index, value) {
+  // ...
+};
+
+LinkedList.prototype.insertAfter = function(node, value) {
+  // ...
+};
+
+LinkedList.prototype.insertBefore = function(node, value) {
   // ...
 };
 
