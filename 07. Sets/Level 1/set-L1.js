7b06b65e624107aee01b00c5d1b61125cbfbe071
Shawn <shawn@ievolved.io>
2017-03-03 09:55:39 -0800

    [update] Enhanced instructions

--- 07. Sets/Level 1/set-L1.js
+++ 07. Sets/Level 1/set-L1.js
@@ -1,13 +1,19 @@
 
 /*
-  A Set is a data structure that can any non-repeating values.  The order of values
+  A Set is a data structure that holds any non-repeating values.  The order of values
    isn't usually important though some implementations my require ordering.  The
    interesting thing about Sets is that usually they are used to merely indicate the
    presence of a value rather than to act as a storage container to later retrieve
-   the value.
+   the value.  While most data structures hold a { key, value } pair, the key is the
+   value in Sets.  Or more precisely, only the key matters.  But a value is needed
+   in Javascript primitive arrays/objects.  Simply using a non-zero number or true
+   would suffice.  In this example, you may choose whatever you like, such as
+   { key = key }, { key = 1 }, { key = true }, etc. but using { key = true } will
+   make it easier to make if (true)... else ... statements.
 
   Sets can be static or dynamic.  As with all static data structures, a static Set is
-   fixed-size.  Data in a Set can be anything: numbers, text, objects, etc.
+   fixed-size.  Data in a Set can be anything: numbers, text, objects, etc.  For this
+   objective, focus only on numbers or text.
 
   Your objective is to complete the implementation of static Set that can add, remove,
    and check for the existence of a value.
